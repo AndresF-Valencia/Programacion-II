117c949ad8b9e45930b8bbce70b7f4d38e6fbc2f
JUAN-PABLO-11 <juanp.espinosap@uqvirtual.edu.co>
2025-02-26 22:00:02 -0500

    Add files via upload
    :D

---
+++ SistemaGestionEmpleados/src/main/java/co/edu/uniquindio/poo/sistemagestionempleados/Gerente.java
@@ -0,0 +1,22 @@
+package co.edu.uniquindio.poo.sistemagestionempleados;
+
+public class Gerente extends Empleado  {
+    public Gerente(String nombre, String id){
+        super(nombre, id);
+    }
+
+    @Override
+    public void ejecutarProyecto() {
+
+    }
+
+    @Override
+    public void realizarPruebas() {
+
+    }
+
+    @Override
+    public void enviarDocumentacion() {
+
+    }
+}
---
+++ SistemaGestionEmpleados/src/main/java/co/edu/uniquindio/poo/sistemagestionempleados/Proyecto.java
@@ -0,0 +1,29 @@
+package co.edu.uniquindio.poo.sistemagestionempleados;
+
+public class Proyecto {
+    private String nombre;
+    private String codigo;
+    Empleado ListaEmpleadosAsignados ;
+
+    public Proyecto(String nombre) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.ListaEmpleadosAsignados = ListaEmpleadosAsignados;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public Empleado getListaEmpleadosAsignados() {
+        return ListaEmpleadosAsignados;
+    }
+
+}
