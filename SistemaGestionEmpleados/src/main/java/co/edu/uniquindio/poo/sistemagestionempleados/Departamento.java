beb576d5e4f6870b4d76b69c560a24b7f1fce0b7
Sebastian-Mateus <juans.mateush@uqvirtual.edu.co>
2025-02-27 11:57:30 -0500

    Modificacion metodos departamento y proyecto
    Se implementan los metodos
    ### *Clase Proyecto*
    - agregarEmpleado(Empleado e): void
    - eliminarEmpleado(Empleado e): void
    - obtenerListaEmpleados(): List<Empleado>
    - cambiarEstado(String estado): void
    
    ### *Clase Departamento*
    - agregarEmpleado(Empleado e): void
    - eliminarEmpleado(Empleado e): void
    - obtenerListaEmpleados(): List<Empleado>
    - asignarGerente(Gerente g): void
    - obtenerGerente(): Gerente

--- SistemaGestionEmpleados/src/main/java/co/edu/uniquindio/poo/sistemagestionempleados/Departamento.java
+++ SistemaGestionEmpleados/src/main/java/co/edu/uniquindio/poo/sistemagestionempleados/Departamento.java
@@ -1,31 +1,53 @@
 package co.edu.uniquindio.poo.sistemagestionempleados;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Departamento {
     private String nombre;
     private String codigo;
-    Empleado listaEmpleado;
+    private List<Empleado> listaEmpleados;
+    private Gerente gerente;
 
-    public Departamento(String nombre) {
+    public Departamento(String nombre, String codigo) {
         this.nombre = nombre;
         this.codigo = codigo;
-        this.listaEmpleado = listaEmpleado;
+        this.listaEmpleados = new ArrayList<>();
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getCodigo() {
         return codigo;
     }
+
     public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
-    public Empleado getListaEmpleado() {
-        return listaEmpleado;
+
+    public List<Empleado> getListaEmpleados() {
+        return listaEmpleados;
     }
-    public void setListaEmpleado(Empleado listaEmpleado) {
-        this.listaEmpleado = listaEmpleado;
+
+    public void agregarEmpleado(Empleado empleado) {
+        listaEmpleados.add(empleado);
+    }
+
+    public void eliminarEmpleado(Empleado empleado) {
+        listaEmpleados.remove(empleado);
+    }
+
+    public void asignarGerente(Gerente gerente) {
+        this.gerente = gerente;
+    }
+
+    public Gerente getGerente() {
+        return gerente;
     }
 }
--- SistemaGestionEmpleados/src/main/java/co/edu/uniquindio/poo/sistemagestionempleados/Proyecto.java
+++ SistemaGestionEmpleados/src/main/java/co/edu/uniquindio/poo/sistemagestionempleados/Proyecto.java
@@ -1,29 +1,46 @@
 package co.edu.uniquindio.poo.sistemagestionempleados;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Proyecto {
     private String nombre;
     private String codigo;
-    Empleado ListaEmpleadosAsignados ;
+    private String estado;
+    private List<Empleado> listaEmpleadosAsignados;
 
-    public Proyecto(String nombre) {
+    public Proyecto(String nombre, String codigo) {
         this.nombre = nombre;
         this.codigo = codigo;
-        this.ListaEmpleadosAsignados = ListaEmpleadosAsignados;
+        this.listaEmpleadosAsignados = new ArrayList<>();
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getCodigo() {
         return codigo;
     }
+
     public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
-    public Empleado getListaEmpleadosAsignados() {
-        return ListaEmpleadosAsignados;
+    
+    public List<Empleado> getListaEmpleadosAsignados() {
+        return listaEmpleadosAsignados;
     }
 
+    public void agregarEmpleado(Empleado empleado) {
+        listaEmpleadosAsignados.add(empleado);
+    }
+
+    public void eliminarEmpleado(Empleado empleado) {
+        listaEmpleadosAsignados.remove(empleado);
+    }
 }
+
